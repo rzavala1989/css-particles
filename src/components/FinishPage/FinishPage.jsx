a95c6fa1c1d7e2332d06560148ebe59818696a93
Ricardo Zavala <zavala.ricardo.m@gmail.com>
2022-11-14 11:40:21 -0800

    init commit

---
+++ src/components/FinishPage/FinishPage.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const FinishPage = () => {
+  return <div>FinishPage</div>;
+};
---
+++ src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './style.css';
+
+export const LandingPage = () => {
+  return <div>LandingPage</div>;
+};
